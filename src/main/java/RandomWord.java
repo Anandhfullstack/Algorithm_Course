6a480e738fecd7119dc5ce00c90efe8a7b673cb2
Anandhfullstack <ananthagopi003@gmail.com>
2023-02-26 21:22:31 +0800

    First Assignment got 100%

--- src/main/java/RandomWord.java
+++ src/main/java/RandomWord.java
@@ -1,10 +1,8 @@
-package org.example;
 import edu.princeton.cs.algs4.StdIn;
 import edu.princeton.cs.algs4.StdRandom;
 
 public class RandomWord {
     public static void main(String[] args){
-
         String champion = "";
         int i =0;
         while(!StdIn.isEmpty()){
